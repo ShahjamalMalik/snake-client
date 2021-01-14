3657c61c1e9a21098f03979464f4c750966fdb24
ShahjamalMalik <shahjamalmalik@gmail.com>
2021-01-14 22:20:00 +0000

    Sending server the name of the snake in the form of a conn.write inside of our connect event callback

--- client.js
+++ client.js
@@ -8,11 +8,16 @@ const connect = function() {
 
   conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+    console.log("Successfully connected!");
+    conn.write("Name: SAM");
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
-  })
+  });
 
   return conn;
-}
+};
 
 module.exports = { connect };
