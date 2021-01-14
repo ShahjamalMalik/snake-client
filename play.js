9fca8e2738e171da8cecd2b6e73facae1b3004d6
ShahjamalMalik <shahjamalmalik@gmail.com>
2021-01-14 22:47:51 +0000

    Created a input.js file to have our two recently created functions moved into there and then exported to play.js for cleanup!

--- play.js
+++ play.js
@@ -1,22 +1,8 @@
-const handleUserInput = function(inputOfUser) {
-  if(inputOfUser === '\u0003') {
-    process.exit();
-  }
-}
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
-
-setupInput();
-
-
-
+const { setupInput } = require('./input.js')
 const { connect } = require('./client.js');
+
+
 console.log('Connecting ...');
 connect();
+
+setupInput();
